42356bad864020b7bf6f30600ba5c9899ba0ea34
Stephen Barr <stephen@vertexdev.com.au>
2020-02-06 11:17:24 +1000

    Reverting back to GFM

--- render.sh
+++ render.sh
@@ -4,6 +4,6 @@ cd /wdir
 mkdir -p out
 cat *.md > out/tmp.md
 ls /root/.pandoc/templates
-pandoc --from=markdown_github --to=latex --template /root/.pandoc/templates/eisvogel.latex --output out/report.pdf -V titlepage -V title:"Australian Aeronautics Technical Manual" -V toc-own-page -V titlepage-color:"2e9fff" --toc out/tmp.md
+pandoc --from=gfm --to=latex --template /root/.pandoc/templates/eisvogel.latex --output out/report.pdf -V titlepage -V title:"Australian Aeronautics Technical Manual" -V toc-own-page -V titlepage-color:"2e9fff" --toc out/tmp.md
 
 
