a3673430c8c8dd4d2f58e8cd4b709f7c5f786695
Stephen Barr <stephen@vertexdev.com.au>
2020-02-06 10:32:33 +1000

    Adding dx line

--- render.sh
+++ render.sh
@@ -3,6 +3,7 @@
 cd /wdir
 mkdir -p out
 cat *.md > out/tmp.md
+ls /root/.pandoc/templates
 pandoc --from=markdown_github --to=latex --template /root/.pandoc/templates/eisvogel.latex --output out/report.pdf -V titlepage -V title:"Australian Aeronautics Technical Manual" -V toc-own-page -V titlepage-color:"2e9fff" --toc out/tmp.md
 
 
