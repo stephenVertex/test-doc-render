dea367dd29383a1997ec89d73ef006fd18cb4ac7
Stephen Barr <stephen@vertexdev.com.au>
2020-02-06 10:25:24 +1000

    No GFM reader? Weird

--- render.sh
+++ render.sh
@@ -3,4 +3,5 @@
 cd /wdir
 mkdir -p out
 cat *.md > out/tmp.md
-pandoc --from=gfm --to=latex --template ~/.pandoc/templates/eisvogel --output out/report.pdf -V titlepage -V title:"Australian Aeronautics Technical Manual" -V toc-own-page -V titlepage-color:"2e9fff" --toc out/tmp.md
+pandoc --from=markdown_github --to=latex --template ~/.pandoc/templates/eisvogel --output out/report.pdf -V titlepage -V title:"Australian Aeronautics Technical Manual" -V toc-own-page -V titlepage-color:"2e9fff" --toc out/tmp.md
+
