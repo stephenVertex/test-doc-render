e464a6ae8be04bdaefdbb3fff75c1759fa8f82be
Stephen Barr <stephen@vertexdev.com.au>
2020-02-06 10:27:03 +1000

    Fixing the template placement

--- render.sh
+++ render.sh
@@ -3,5 +3,6 @@
 cd /wdir
 mkdir -p out
 cat *.md > out/tmp.md
-pandoc --from=markdown_github --to=latex --template ~/.pandoc/templates/eisvogel --output out/report.pdf -V titlepage -V title:"Australian Aeronautics Technical Manual" -V toc-own-page -V titlepage-color:"2e9fff" --toc out/tmp.md
+pandoc --from=markdown_github --to=latex --template /root/.pandoc/templates/eisvogel.latex --output out/report.pdf -V titlepage -V title:"Australian Aeronautics Technical Manual" -V toc-own-page -V titlepage-color:"2e9fff" --toc out/tmp.md
+
 
